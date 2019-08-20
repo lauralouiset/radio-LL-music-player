f90f52dab7750ee824aafd21a146f5ecd9b0a520
Laura-Louise Tobin <lauralouise@lauraloui.se>
2019-08-19 22:05:33 -0400

    Initial commit from Create React App

---
+++ src/components/audioPlayer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AudioPlayer extends Component {
+  render() {
+    return (
+      <div>
+        <audio src="" />
+      </div>
+    )
+  }
+}
+
+export default AudioPlayer
